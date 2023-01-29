2501078de11e499b616b66ce0e4b07d37cc943a0
Haider Ali Khan <haiderk0987@gmail.com>
2023-01-29 14:37:21 +0500

    test: wsgi compatibility

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from . import db, auth, blog
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(*args, **kwargs):
+	print(args)
+	print(kwargs)
 	# create and configure the app
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_mapping(
@@ -11,6 +13,7 @@ def create_app(test_config=None):
 		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 	)
 
+	test_config = kwargs.get('test_config')
 	if test_config is None:
 		# load the instance config, if it exists, when not testing
 		app.config.from_pyfile('config.py', silent=True)
