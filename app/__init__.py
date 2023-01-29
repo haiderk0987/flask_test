f8b1f0b63436675be23f21c3c7b3adc0dc637100
Haider Ali Khan <haiderk0987@gmail.com>
2023-01-29 14:45:27 +0500

    test: revert changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,7 @@ from . import db, auth, blog
 from flask import Flask
 
 
-def create_app(*args, **kwargs):
-	print(args)
-	print(kwargs)
+def create_app(test_config=None):
 	# create and configure the app
 	app = Flask(__name__, instance_relative_config=True)
 	app.config.from_mapping(
@@ -13,7 +11,6 @@ def create_app(*args, **kwargs):
 		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 	)
 
-	test_config = kwargs.get('test_config')
 	if test_config is None:
 		# load the instance config, if it exists, when not testing
 		app.config.from_pyfile('config.py', silent=True)
